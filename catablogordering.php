713092c9126508cf6b8ebfd2eb13a532529a7d7f
diego2k <diego2k@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-12 15:26:33 +0000

    First Public Release
    git-svn-id: http://plugins.svn.wordpress.org/catablog-ordering/trunk@530280 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ catablogordering.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Plugin Name: Catablog Ordering
+Plugin URI: https://github.com/genus/catablogordering
+Description: Add Catablog Plugin Support for Making Purchase Orders
+Version: 0.1
+Author: diego2k
+Author URI: http://www.estudiogenus.com/
+
+Copyright 2012  Diego Coppari (email: diego2k@gmail.com)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+
+// Make sure we don't expose any info if called directly
+if ( !function_exists( 'add_action' ) ) 
+{
+    echo "Hi there!  I'm just a plugin, not much I can do when called directly.";
+    exit;
+}
+                
+require('CataBlogOrdering.class.php');
+
+load_plugin_textdomain('catablogcart', false, '/catablogcart/lang');
+
+add_action('admin_menu', array('CataBlogCart', 'admin_settings_menu') );
+
+add_shortcode('catablogcart', array('CataBlogCart', 'showCart'));
+
+wp_enqueue_style('catablog-cart-css', plugins_url( 'css/catablog-cart.css', __FILE__ ), false, '1','all');
+
+add_action('init', array('CataBlogCart', 'processEvent'), 10);
+
